3db8126ae57ae28ae551da925365015a18b90979
amit.rozen <amitrozen11@gmail.com>
2024-09-12 09:39:15 +0300

    add data in the server dise about scheme of the animals and the http requests

---
+++ server/controllers/animalController.js
@@ -0,0 +1,69 @@
+const Animal = require('../models/animalController');
+
+// get all animals
+const getAllAnimals = async (req, res) => {
+    try {
+        const animal = await Animal.find();
+        res.status(200).json({animal});
+    } catch (err) {
+        res.status(400).json({mssg: 'error getting animal', err})
+    }
+}
+
+
+// get a single animal
+const getSingleAnimal = async (req, res) => {
+    const {animal_id} = req.params;
+
+    try {
+        const animal = await Animal.findById(animal_id);
+        res.status(200).json({animal});
+    } catch (err) {
+        res.status(400).json({mssg: 'error getting animal', err})
+    }
+}
+
+// create a new animal
+const createAnimal = async (req, res) => {
+    const {animal_id, name, age, sex, animal_type, images_and_videos, description, contact_user, area_of_adoption, color} = req.body;
+
+    try {
+        const animal = await Animal.create({animal_id, name, age, sex, animal_type, images_and_videos, description, contact_user, area_of_adoption, color});
+        res.status(200).json({animal});
+    } catch (err) {
+        res.status(400).json({mssg: 'error creating animal', err})
+    }
+}
+
+// delete a animal
+const deleteAnimal = async (req, res) => {
+    const {animal_id} = req.params;
+
+    try {
+        const animal = await Animal.findByIdAndDelete(animal_id);
+        res.status(200).json({animal});
+    } catch (err) {
+        res.status(400).json({mssg: 'error deleting animal', err})
+    }
+}
+
+// update a animal
+const updateAnimal = async (req, res) => {
+    const {animal_id} = req.params;
+    const {name, age, sex, animal_type, images_and_videos, description, contact_user, area_of_adoption, color} = req.body;
+
+    try {
+        const animal = await Animal.findByIdAndUpdate(id, {name, age, sex, animal_type, images_and_videos, description, contact_user, area_of_adoption, color}, {new: true});
+        res.status(200).json({animal});
+    } catch (err) {
+        res.status(400).json({mssg: 'error updating animal', err})
+    }
+}
+
+module.exports = {
+    createAnimal,
+    getAllAnimals,
+    getSingleAnimal,
+    deleteAnimal,
+    updateAnimal,
+}
