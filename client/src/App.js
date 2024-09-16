b071262769baa27e40775e9e6b5c7179891d6e46
galAlpert <galalpert5@gmail.com>
2024-09-16 16:08:28 +0300

    added new folder for the upload page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/HomePage/HomePage';
-import UploadAnimal from './pages/HomePage/UploadAnimal';
+import UploadAnimal from './pages//UploadAnimalPage/UploadAnimal';
 import styles from './styles/App.module.css';
 
 function App() {
