b6c3cf9633f0ac8e495e2089b68e5203dc2cd869
amit.rozen <amitrozen11@gmail.com>
2024-09-26 12:11:32 +0300

    order the home page

--- client/src/components/AnimalItem/AnimalItem.js
+++ client/src/components/AnimalItem/AnimalItem.js
@@ -1,8 +1,10 @@
 const AnimalItem = ({ animals }) => {
     return (
         <div className="animal-item">
-            
+
             <h4>{animals.name}</h4>
+            <img src={animals.images_and_videos} alt="photo"></img>
+            <p>{animals.images_and_videos}</p>
             <p><strong>animal type: </strong>{animals.animal_type}</p>
             <p><strong>color: </strong>{animals.color}</p>
             <p><strong>sex: </strong>{animals.sex}</p>
