9af6a6e1e850697b7bce3f3583c028919b5e6b0c
amit.rozen <amitrozen11@gmail.com>
2024-09-29 16:47:35 +0300

    another changes

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from '../../styles/global.css';
 import DisplayAnimals from '../../components/DisplayAnimals/DisplayAnimals.js'
 
+//      <h1 className={styles.headline}>As soon as you click on it, a tab will open for you</h1>
+
 const Home = () => {
   return (
     <div className={styles.home}>
       <h1 className={styles.headline}>Choose the animal that interests you!</h1>
-      <h1 className={styles.headline}>As soon as you click on it, a tab will open for you</h1>
         <DisplayAnimals/>
     </div>
   );
