2b374705cedad6fa55ca62bb55adce6592f6b38d
amit.rozen <amitrozen11@gmail.com>
2024-09-29 23:51:15 +0300

    add a logo as home button and order the image on start dashboard

--- client/src/components/AnimalItem/AnimalItem.js
+++ client/src/components/AnimalItem/AnimalItem.js
@@ -1,25 +1,12 @@
-import { AnimalContext } from '../../context/AnimalContext';
 
 const AnimalItem = ({ animals }) => {
-    /*const {dispatch} = AnimalContext()
 
-    const handleClick = async() => {
-        const response = await fetch('/animal' + animals.animal_id, {
-            method: 'DELETE'
-        })
-        const json = await response.json()
-
-        if (response.ok) {
-            dispatch({type: 'DELETE_ANIMAL', payload: json})
-        }
-    }*/ //delete func
-    //<span onClick={handleClick}>delete</span>
 
     return (
         <div className="animal-item">
 
             <h1>{animals.name}</h1>
-            <img src={animals.images_and_videos} alt={animals.title} /*className={styles.img}*/></img>
+            <img src={`http://localhost:5000/server/AnimalUploadMedia/${animals.images_and_videos[0]}`} alt={animals.name} />
             <p><strong>animal type: </strong>{animals.animal_type}</p>
             <p><strong>color: </strong>{animals.color}</p>
             <p><strong>sex: </strong>{animals.sex}</p>
