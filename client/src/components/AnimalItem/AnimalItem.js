c8d76a6e5a19d7a14805bd10fd6c72c7d80d90aa
amit.rozen <amitrozen11@gmail.com>
2024-09-30 09:16:23 +0300

    changes

--- client/src/components/AnimalItem/AnimalItem.js
+++ client/src/components/AnimalItem/AnimalItem.js
@@ -6,7 +6,10 @@ const AnimalItem = ({ animals }) => {
         <div className="animal-item">
 
             <h1>{animals.name}</h1>
-            <img src={`http://localhost:5000/server/AnimalUploadMedia/${animals.images_and_videos[0]}`} alt={animals.name} />
+            <img 
+                src={`http://localhost:5000/uploads/${animals.animal_id}_${animals.images_and_videos[0]}`} 
+                alt={animals.images_and_videos} 
+                className="image"/>
             <p><strong>animal type: </strong>{animals.animal_type}</p>
             <p><strong>color: </strong>{animals.color}</p>
             <p><strong>sex: </strong>{animals.sex}</p>
